8587010509afffa2ac95e0c29bc24ef3d41ebd54
Ruben E Acosta <reacosta@grupo-exito.com>
2024-06-07 13:51:29 -0500

    meta data home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,10 @@
 import { MainProducts } from "app/components/home/MainProducts"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: 'Future world',
+  description: 'Welcome to the future world, an ecommerce from other time.'
+}
 
 export default function Home() {
   return (
